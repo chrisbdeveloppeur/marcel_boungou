7992298460a0c9aa8e416b174246a19d60f66f1c
K4gur0 <kenshin91cb@gmail.com>
2022-01-24 16:00:12 +0100

    LAPTOP add translator

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\Date;
 
 /**
  * @Route("/", name="home_")
@@ -16,8 +17,9 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $hightlights_dates = [strval(date("d/m/Y", time() - 60 * 60 * 24)),strval(date("d/m/Y", time() + 60 * 60 * 24))];
         return $this->render('includes/calendar.html.twig', [
-            'controller_name' => 'HomeController',
+            'hightlights_dates' => $hightlights_dates,
         ]);
     }
 }
