d6b729c53c35be139f17d87dc96dc8f98f102f89
chrisbdevelopper <kenshin91cb@gmail.com>
2022-02-11 22:31:48 +0100

    DESKTOP modif front end

--- assets/app.js
+++ assets/app.js
@@ -13,6 +13,7 @@ const fr_flag = require('./images/svg/fr.svg');
 const en_flag = require('./images/svg/en.svg');
 const palata_1 = require('./images/jpg/MB/palatasingers1.jpg');
 const palata_2 = require('./images/jpg/MB/palatasingers2.jpg');
+const mb_1 = require('./images/jpg/MB/mb_cost_blue_1.jpg');
 const sunset = require('./images/jpg/sunset.jpg');
 const sunset_pecheur = require('./images/jpg/sunset_pecheur.jpg');
 const clouds = require('./images/jpg/clouds.jpg');
