f897cf598ad0d7b5aba712f699afd6660f2a4300
K4gur0 <kenshin91cb@gmail.com>
2022-02-21 20:00:45 +0100

    LAPTOP concretisation music entity + CRUD

--- migrations/Version20220221183135.php
+++ migrations/Version20220221183135.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220221150844 extends AbstractMigration
+final class Version20220221183135 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220221150844 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event ADD tags LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\'');
+        $this->addSql('ALTER TABLE music ADD music_name VARCHAR(255) DEFAULT NULL, ADD created_date DATE DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE event DROP tags');
+        $this->addSql('ALTER TABLE music DROP music_name, DROP created_date');
     }
 }
--- src/Entity/Music.php
+++ src/Entity/Music.php
@@ -4,6 +4,10 @@ namespace App\Entity;
 
 use App\Repository\MusicRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=MusicRepository::class)
@@ -26,6 +30,39 @@ class Music
      * @ORM\Column(type="integer", nullable=true)
      */
     private $duration;
+
+    /**
+     *
+     * @Vich\UploadableField(mapping="music_file", fileNameProperty="musicName")
+     *
+     * @var File|null
+     * @Assert\File(
+     *     maxSize="100Mi",
+     *     mimeTypes={"audio/mpeg","application/octet-stream","application/x-font-gdos","audio/x-wav"})
+     */
+    private $musicFile;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     *
+     * @var string|null
+     */
+    private $musicName;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $createdDate;
+
+    public function __construct()
+    {
+        $this->createdDate = new \DateTime('now');
+    }
+
+    public function __toString()
+    {
+        return $this->titre;
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +89,57 @@ class Music
     public function setDuration(?int $duration): self
     {
         $this->duration = $duration;
+
+        return $this;
+    }
+
+    /**
+     * @return File|null
+     */
+    public function getMusicFile(): ?File
+    {
+        return $this->musicFile;
+    }
+
+    /**
+     * @param File|null $musicFile
+     * @return Music
+     */
+    public function setMusicFile(?File $musicFile): Music
+    {
+        $this->musicFile = $musicFile;
+        if ($this->musicFile instanceof UploadedFile) {
+            $this->updatedAt = new \DateTime('now');
+        }
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getMusicName(): ?string
+    {
+        return $this->musicFile;
+    }
+
+    /**
+     * @param string|null $musicName
+     * @return Music
+     */
+    public function setMusicName(?string $musicName): Music
+    {
+        $this->musicName = $musicName;
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeInterface
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(?\DateTimeInterface $createdDate): self
+    {
+        $this->createdDate = $createdDate;
 
         return $this;
     }
